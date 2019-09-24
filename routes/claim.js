3affaf8fed7d4a188f40d9b776e30ce00a70487e
cliu3322 <44553949+cliu3322@users.noreply.github.com>
2019-09-24 08:51:56 +0700

    fix insert documents

--- routes/claim.js
+++ routes/claim.js
@@ -30,176 +30,6 @@ router.get('/', function(req, res, next) {
 });
 
 
-router.post('/insertClaim', awaitErorrHandlerFactory(async (req, res, next) => {
-  const { plan_name, member_policy_name, mode_of_treatment, date_of_hospitalisation_visit,
-  cause_of_hospitalisation_visit, hospital_name_address, location, doctor_name,
-  billing_currency, reimbursement_currency, email, phone, home_address, bank_account_number,
-  account_holder_name, bank_name, bank_address, swift_address, iban_code } = req.body;
-
-  const response = {};
-  // You can use DB checking here
-  //doesUserEverExists
-
-
-
-  try {
-    const policyCount = await models.ClaimBasic.count({})
-
-
-    const input = await models.ClaimBasic.create({ plan_name, member_policy_name, mode_of_treatment, date_of_hospitalisation_visit,
-    cause_of_hospitalisation_visit, hospital_name_address, location, doctor_name,
-    billing_currency, reimbursement_currency, email, phone, home_address, bank_account_number,
-    account_holder_name, bank_name, bank_address, swift_address, iban_code});
-
-    response.claimID = input.dataValues.id;
-  } catch(e) {
-    console.log(e)
-    response.error = e;
-  }
-  res.json(response);
-}));
-
-
-
-
-router.post('/uploaddocument', awaitErorrHandlerFactory(async (req, res, next) => {
-  var form = new formidable.IncomingForm();
-  form.uploadDir = "../upload/documents";
-  form.keepExtensions = true;
-  form.multiples = true;
-  form.parse(req);
-
-  var files = []
-  var data = {}
-  form.on('file', function (name, file){
-    files.push(file)
-  });
-  //https://stackoverflow.com/questions/34264800/node-js-function-return-object-object-instead-of-a-string-value
-  form.on('field', function(name, value) {
-    data[name]= value;
-  });
-
-  form.on('fileBegin', function(name, file) {
-
-  });
-
-  form.on('end',async function() {
-    try {
-
-      if(files.length > 1)
-        throw new Error('file length is longer than 1');
-
-      const fileRecord = await models.DocumentsFiles.create({fileName:files[0].name, path: files[0].path, visiId: data.visitId, active:true})
-      res.send(fileRecord);
-    } catch(e) {
-      console.log(e)
-      res.sendStatus(500)
-    }
-
-    // fs.rename(files[0].path, form.uploadDir+'/'+visit.claimID, function(err) {
-    //     if (err) next(err);
-    // });
-  });
-
-}));
-
-router.post('/uploadBillingInfo', awaitErorrHandlerFactory(async (req, res, next) => {
-  //https://github.com/node-formidable/node-formidable/issues/260
-  var form = new formidable.IncomingForm();
-  form.uploadDir = "../upload/billinginfo";
-  form.keepExtensions = true;
-  form.multiples = true;
-  form.parse(req);
-
-  var files = []
-  var fields = {}
-  form.on('file', function (name, file){
-    files.push(file)
-  });
-  //https://stackoverflow.com/questions/34264800/node-js-function-return-object-object-instead-of-a-string-value
-  form.on('field', function(name, value) {
-    fields[name]= value;
-  });
-
-  form.on('fileBegin', function(name, file) {
-
-  });
-
-  form.on('end',async function() {
-    try {
-
-
-      //upload files
-      for (var i = 0; i < files.length; i++) {
-        var extension = path.extname(files[i].name).toLowerCase();
-        var newFileName = fields.visitId.replace(/\//g,'_')+'-'+i+extension
-        fs.rename(files[i].path, form.uploadDir+'/'+newFileName, function(err) {
-            if (err) next(err);
-        });
-
-        const fileRecord = await models.BillingInfoFiles.create(
-          {fileName:newFileName, fileAddress: form.uploadDir, visitId: fields.visitId}
-        )
-
-      }
-
-      //upload currency info
-
-      await models.ClaimInfoVisits.findByPk(fields.visitId).then((visit) => {
-
-        return visit.update(JSON.parse(fields.currency));
-      })
-
-      //insert billingInfo
-      var billingInfo = JSON.parse(fields.billingInfo)
-
-      for (var i = 0; i < billingInfo.length; i++) {
-
-        billingInfo[i].visitId=fields.visitId
-        await models.BillingInfo.create(billingInfo[i]);
-
-      }
-
-      res.sendStatus(200)
-
-    }
-    catch(e) {
-      console.log(e)
-      res.sendStatus(500)
-    }
-
-    // fs.rename(files[0].path, form.uploadDir+'/'+visit.claimID, function(err) {
-    //     if (err) next(err);
-    // });
-  });
-
-}));
-
-router.post('/insertdocument', awaitErorrHandlerFactory(async (req, res, next) => {
-
-  try {
-    console.log(req.body.path)
-
-     models.DocumentsFiles.describe().then(function (schema) {
-        return Object.keys(schema).filter(function(field){
-            return schema[field].primaryKey;
-        });
-    }).tap(console.log);
-
-    // const result = await models.BillingInfoFiles.findByPk(req.body.path).then((BillingInfoFile) => {
-    //   console.log(BillingInfoFile)
-    //   //return BillingInfoFiles.update({notes:req.body.notes});
-    // })
-    // console.log('reuslt',result)
-    res.sendStatus(200)
-
-  }
-  catch(e) {
-    console.log(e)
-    res.sendStatus(500)
-  }
-}));
-
 
 router.post('/insertClaimInfo', awaitErorrHandlerFactory(async (req, res, next) => {
   const response = {};
@@ -279,6 +109,141 @@ router.post('/insertClaimInfoVisits', (req, res, next) => {
   });
 
 })
+
+
+router.post('/insertBillingInfo', awaitErorrHandlerFactory(async (req, res, next) => {
+  //https://github.com/node-formidable/node-formidable/issues/260
+  var form = new formidable.IncomingForm();
+  form.uploadDir = "../upload/billinginfo";
+  form.keepExtensions = true;
+  form.multiples = true;
+  form.parse(req);
+
+  var files = []
+  var fields = {}
+  form.on('file', function (name, file){
+    files.push(file)
+  });
+  //https://stackoverflow.com/questions/34264800/node-js-function-return-object-object-instead-of-a-string-value
+  form.on('field', function(name, value) {
+    fields[name]= value;
+  });
+
+  form.on('fileBegin', function(name, file) {
+
+  });
+
+  form.on('end',async function() {
+    try {
+
+
+      //upload files
+      for (var i = 0; i < files.length; i++) {
+        var extension = path.extname(files[i].name).toLowerCase();
+        var newFileName = fields.visitId.replace(/\//g,'_')+'-'+i+extension
+        fs.rename(files[i].path, form.uploadDir+'/'+newFileName, function(err) {
+            if (err) next(err);
+        });
+
+        const fileRecord = await models.BillingInfoFiles.create(
+          {fileName:newFileName, fileAddress: form.uploadDir, visitId: fields.visitId}
+        )
+
+      }
+
+      //upload currency info
+
+      await models.ClaimInfoVisits.findByPk(fields.visitId).then((visit) => {
+
+        return visit.update(JSON.parse(fields.currency));
+      })
+
+      //insert billingInfo
+      var billingInfo = JSON.parse(fields.billingInfo)
+
+      for (var i = 0; i < billingInfo.length; i++) {
+
+        billingInfo[i].visitId=fields.visitId
+        await models.BillingInfo.create(billingInfo[i]);
+
+      }
+
+      res.sendStatus(200)
+
+    }
+    catch(e) {
+      console.log(e)
+      res.sendStatus(500)
+    }
+
+    // fs.rename(files[0].path, form.uploadDir+'/'+visit.claimID, function(err) {
+    //     if (err) next(err);
+    // });
+  });
+
+}));
+
+
+router.post('/uploaddocument', awaitErorrHandlerFactory(async (req, res, next) => {
+  var form = new formidable.IncomingForm();
+  form.uploadDir = "../upload/documents";
+  form.keepExtensions = true;
+  form.multiples = true;
+  form.parse(req);
+
+  var files = []
+  var data = {}
+  form.on('file', function (name, file){
+    files.push(file)
+  });
+  //https://stackoverflow.com/questions/34264800/node-js-function-return-object-object-instead-of-a-string-value
+  form.on('field', function(name, value) {
+    data[name]= value;
+  });
+
+  form.on('fileBegin', function(name, file) {
+
+  });
+
+  form.on('end',async function() {
+    try {
+
+      if(files.length > 1)
+        throw new Error('file length is longer than 1');
+
+      const fileRecord = await models.DocumentsFiles.create({fileName:files[0].name, path: files[0].path, visitId: data.visitId, active:true})
+      res.send(fileRecord);
+    } catch(e) {
+      console.log(e)
+      res.sendStatus(500)
+    }
+
+    // fs.rename(files[0].path, form.uploadDir+'/'+visit.claimID, function(err) {
+    //     if (err) next(err);
+    // });
+  });
+
+}));
+
+router.post('/insertdocumentsnotes', awaitErorrHandlerFactory(async (req, res, next) => {
+  const response = {};
+  try {
+    console.log('req.body.path', req.body.path)
+    const result = await models.DocumentsFiles.findByPk(req.body.path).then((DocumentsFile) => {
+      console.log(DocumentsFile)
+      return DocumentsFile.update({notes:req.body.notes});
+    })
+    console.log(result.id)
+
+  } catch(e) {
+    console.log(e)
+    response.error = e;
+    res.sendStatus(500)
+  }
+
+  res.json(response);
+}));
+
 
 
 
