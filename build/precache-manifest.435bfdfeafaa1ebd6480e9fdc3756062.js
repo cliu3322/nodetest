c7c51954d20678ea5d82d79792142558240f5e42
cliu3322 <44553949+cliu3322@users.noreply.github.com>
2019-12-02 09:27:56 +0700

    UI small change according to ticket && yarn upgrade

---
+++ build/precache-manifest.435bfdfeafaa1ebd6480e9fdc3756062.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff99da7a0801f16d39a7caec817ecc3c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e4ce66eaf31d46efdb4",
+    "url": "/static/css/0.4cf12f64.chunk.css"
+  },
+  {
+    "revision": "b6201f19b6b8c40fdbbe",
+    "url": "/static/css/1.711b031c.chunk.css"
+  },
+  {
+    "revision": "58ad5ad026dfb0f4444e",
+    "url": "/static/css/10.7c6fb41e.chunk.css"
+  },
+  {
+    "revision": "69f419dfc221d437418e",
+    "url": "/static/css/12.db477c03.chunk.css"
+  },
+  {
+    "revision": "9147a42f2e838b87d4e4",
+    "url": "/static/css/13.497e6e98.chunk.css"
+  },
+  {
+    "revision": "a7a469b64e04db66c7d8",
+    "url": "/static/css/14.b75ac8fa.chunk.css"
+  },
+  {
+    "revision": "4482deb960c0b036a403",
+    "url": "/static/css/15.eb0f9185.chunk.css"
+  },
+  {
+    "revision": "27f0fe3d43e4bb613d40",
+    "url": "/static/css/2.29821652.chunk.css"
+  },
+  {
+    "revision": "7aca29a869995eaea090",
+    "url": "/static/css/3.fdcc8277.chunk.css"
+  },
+  {
+    "revision": "7457b6da2c217c22efd3",
+    "url": "/static/css/4.a284e85a.chunk.css"
+  },
+  {
+    "revision": "d44cf7ba095adaca70ba",
+    "url": "/static/css/5.ea4c15ba.chunk.css"
+  },
+  {
+    "revision": "0cc6721b52b8edf7377d",
+    "url": "/static/css/main.35463903.chunk.css"
+  },
+  {
+    "revision": "9e4ce66eaf31d46efdb4",
+    "url": "/static/js/0.eac4334d.chunk.js"
+  },
+  {
+    "revision": "b6201f19b6b8c40fdbbe",
+    "url": "/static/js/1.b2d4e978.chunk.js"
+  },
+  {
+    "revision": "58ad5ad026dfb0f4444e",
+    "url": "/static/js/10.3cb4605d.chunk.js"
+  },
+  {
+    "revision": "b4526f64964e02702f8e",
+    "url": "/static/js/11.1b1f1de0.chunk.js"
+  },
+  {
+    "revision": "69f419dfc221d437418e",
+    "url": "/static/js/12.4f2e4ddf.chunk.js"
+  },
+  {
+    "revision": "9147a42f2e838b87d4e4",
+    "url": "/static/js/13.3076a1cf.chunk.js"
+  },
+  {
+    "revision": "a7a469b64e04db66c7d8",
+    "url": "/static/js/14.85fcd449.chunk.js"
+  },
+  {
+    "revision": "4482deb960c0b036a403",
+    "url": "/static/js/15.b7b75650.chunk.js"
+  },
+  {
+    "revision": "922a79cf1001a8c7c08d",
+    "url": "/static/js/16.f2abefc4.chunk.js"
+  },
+  {
+    "revision": "17cf03ba6240b7328534",
+    "url": "/static/js/17.c364a52c.chunk.js"
+  },
+  {
+    "revision": "c401f85d627f57a0282d",
+    "url": "/static/js/18.9f428c97.chunk.js"
+  },
+  {
+    "revision": "146eb809472009a6a95c",
+    "url": "/static/js/19.f9f706cd.chunk.js"
+  },
+  {
+    "revision": "27f0fe3d43e4bb613d40",
+    "url": "/static/js/2.0c73f6a2.chunk.js"
+  },
+  {
+    "revision": "7aca29a869995eaea090",
+    "url": "/static/js/3.18add6bb.chunk.js"
+  },
+  {
+    "revision": "7457b6da2c217c22efd3",
+    "url": "/static/js/4.fc8cd05e.chunk.js"
+  },
+  {
+    "revision": "d44cf7ba095adaca70ba",
+    "url": "/static/js/5.0a69c068.chunk.js"
+  },
+  {
+    "revision": "e67a11fd230666fc9b1e",
+    "url": "/static/js/6.4ab1b1bb.chunk.js"
+  },
+  {
+    "revision": "bff4a110503f956a41ce",
+    "url": "/static/js/compose-mAIL--editor.3a73b9af.chunk.js"
+  },
+  {
+    "revision": "0cc6721b52b8edf7377d",
+    "url": "/static/js/main.35140a98.chunk.js"
+  },
+  {
+    "revision": "21a2b78e83a330d385fb",
+    "url": "/static/js/runtime-main.77d1aa42.js"
+  },
+  {
+    "revision": "160e7efb5c0f5cb6a55187c732dffe85",
+    "url": "/static/media/allianceLogo.160e7efb.jpg"
+  },
+  {
+    "revision": "4e4d59b7a0903071d743973f8e3b3aab",
+    "url": "/static/media/china.4e4d59b7.svg"
+  },
+  {
+    "revision": "bcd389debadb2cf1c0332df62b80c1b9",
+    "url": "/static/media/france.bcd389de.svg"
+  },
+  {
+    "revision": "604345b476aa52a0245ee2ffd3cd50db",
+    "url": "/static/media/italy.604345b4.svg"
+  },
+  {
+    "revision": "07ba7580a3bd17730bdfd7e8641885ac",
+    "url": "/static/media/signClaim.07ba7580.webp"
+  },
+  {
+    "revision": "6a9dc11ff55791478b31156cd09b6b38",
+    "url": "/static/media/spain.6a9dc11f.svg"
+  },
+  {
+    "revision": "bc48afcc15d5d9d51255de0b2ee708be",
+    "url": "/static/media/uk.bc48afcc.svg"
+  },
+  {
+    "revision": "56bf912220fcc0ea7d0f6595a28f9a4d",
+    "url": "/static/media/work.56bf9122.jpg"
+  }
+]);
